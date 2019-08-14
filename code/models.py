b65362a10b120c65ed411dd5bd1a5466815ccc16
Shantanu Chandra <shantanu.chandra@braincreators.com>
2019-08-14 12:47:38 +0200

    Bug fix with tensorboard and model saving

--- code/models.py
+++ code/models.py
@@ -1,4 +1,3 @@
-
 import torch
 from torch import nn
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
--- code/train.py
+++ code/train.py
@@ -13,7 +13,8 @@ import numpy as np
 
 import torch
 import torchtext
-from tensorboardX import SummaryWriter
+# from tensorboardX import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 from nltk import word_tokenize
 import nltk
 # nltk.download('punkt')
@@ -30,6 +31,7 @@ from dev_test_evals import model_eval
 
 def prepare_training():
     # Define fields for reading SNLI data
+    start = time.time()
     print("="*80 + "\n\t\t\t\tPreparing Data\n" + "="*80 + "\n")
     print("\n==>> Preparing train-dev-test splits of SNLI data...\n")
     TEXT = Field(sequential=True, tokenize=word_tokenize, lower=True, use_vocab=True, batch_first=False, include_lengths=True)
@@ -50,8 +52,8 @@ def prepare_training():
     TEXT.vocab.vectors[TEXT.vocab.stoi['<unk>']] = torch.mean(TEXT.vocab.vectors, dim=0)
 
     # Define the iterator over the train and valid set
-    train_iter, dev_iter = BucketIterator.splits(datasets=(train, dev),
-                                                   batch_sizes=(args.batch_size, args.batch_size),
+    train_iter, dev_iter, test_iter = BucketIterator.splits(datasets=(train, dev, test),
+                                                   batch_sizes=(config['batch_size'], config['batch_size'], config['batch_size']),
                                                    sort_key=lambda x: x.premise,
                                                    shuffle=True,
                                                    sort_within_batch=True,
@@ -59,16 +61,30 @@ def prepare_training():
     # Custom wrapper over the iterators
     train_batch_loader = SNLIBatchGenerator(train_iter)
     dev_batch_loader = SNLIBatchGenerator(dev_iter)
+    test_batch_loader = SNLIBatchGenerator(test_iter)
 
-    return train_batch_loader, dev_batch_loader, TEXT, LABEL
+    end = time.time()
+    hours, minutes, seconds = calc_elapsed_time(start, end)
+
+    print("\n"+ "-"*50 + "\nTook  {:0>2}:{:0>2}:{:05.2f}  to Prepare Data\n".format(hours,minutes,seconds))
+
+    return train_batch_loader, dev_batch_loader, test_batch_loader, TEXT, LABEL
 
 
+def calc_elapsed_time(start, end):
+    hours, rem = divmod(end-start, 3600)
+    time_hours, time_rem = divmod(end, 3600)
+    minutes, seconds = divmod(rem, 60)
+    time_mins, _ = divmod(time_rem, 60)
+    return int(hours), int(minutes), seconds
 
-def eval_network(model):
+
+def eval_network(model, test = False):
     eval_acc = 0
+    batch_loader = test_batch_loader if test else dev_batch_loader
     model.eval()
     with torch.no_grad():
-        for iters, (premise, hyp, label) in enumerate(dev_batch_loader):
+        for iters, (premise, hyp, label) in enumerate(batch_loader):
             out = model(premise[0], premise[1], hyp[0], hyp[1])
             preds = torch.argmax(out, dim=1)
             accuracy = torch.sum(preds == label, dtype=torch.float32) / out.shape[0]
@@ -80,12 +96,9 @@ def eval_network(model):
 
 def print_stats(epoch, train_loss, train_acc, val_acc, start):
     end = time.time()
-    hours, rem = divmod(end-start, 3600)
-    time_hours, time_rem = divmod(end, 3600)
-    minutes, seconds = divmod(rem, 60)
-    time_mins, _ = divmod(time_rem, 60)
-    print(("Epoch: {}/{},    train_loss: {:.4f},  train_acc = {:.4f}   eval_acc = {}:.4f|   Elapsed Time:  {:0>2}:{:0>2}:{:05.2f}"
-                     .format(epoch, config['max_epoch'], train_loss, train_acc, val_acc, int(hours),int(minutes),seconds)))
+    hours, minutes, seconds = calc_elapsed_time(start, end)
+    print(("Epoch: {}/{},    train_loss: {:.4f},  train_acc = {:.2f}   eval_acc = {:.2f}  | Elapsed Time:  {:0>2}:{:0>2}:{:05.2f}"
+                     .format(epoch, config['max_epoch'], train_loss, train_acc, val_acc, hours,minutes,seconds)))
 
 
 
@@ -104,15 +117,16 @@ def train_network():
     criterion = nn.CrossEntropyLoss()
 
     # Load the checkpoint to resume training if found
-    if os.path.isfile(config['checkpoint_path']):
-        checkpoint = torch.load(config['checkpoint_path'])
+    model_file = os.path.join(config['checkpoint_path'], config['outputmodelname'])
+    if os.path.isfile(model_file):
+        checkpoint = torch.load(model_file)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         start_epoch = checkpoint['epoch'] + 1
         print("\nResuming training from epoch %d with loaded model and optimizer..." % start_epoch)
     else:
         start_epoch = 1
-        print("\nNo Checkpoints found for the chosen model... \nTraining the  ''{}''  model from scratch...".format(config['model_name']))
+        print("\nNo Checkpoints found for the chosen model to reusme training... \nTraining the  ''{}''  model from scratch...".format(config['model_name']))
 
     start = time.time()
     best_val_acc = 0
@@ -123,7 +137,7 @@ def train_network():
     for epoch in range(start_epoch, config['max_epoch']+1):
         train_loss = 0
         train_acc = 0
-        TP, FP, TN, FN = 0, 0, 0, 0
+        # TP, FP, TN, FN = 0, 0, 0, 0
         model.train()
         for iters, (premise, hyp, label) in enumerate(train_batch_loader):
             out = model(premise[0], premise[1], hyp[0], hyp[1])
@@ -139,30 +153,30 @@ def train_network():
             train_acc += accuracy
 
         train_loss = train_loss/iters
-        train_acc = train_acc/iters
+        train_acc = train_acc/iters*100
 
         # Evaluate on test set
-        val_acc = eval_network(model)
+        val_acc = eval_network(model)*100
 
         # print stats
         print_stats(epoch, train_loss, train_acc, val_acc, start)
 
         # write stats to tensorboard
-        TP += ((preds == label).float() * (preds == 1).float()).sum(dim=(0,1)).cpu().data.numpy()
-        FP += ((preds != label).float() * (preds == 1).float()).sum(dim=(0,1)).cpu().data.numpy()
-        TN += ((preds == label).float() * (preds == 0).float()).sum(dim=(0,1)).cpu().data.numpy()
-        FN += ((preds != label).float() * (preds == 0).float()).sum(dim=(0,1)).cpu().data.numpy()
+        # TP += ((preds == label).float() * (preds == 1).float()).sum(dim=(0,1)).cpu().data.numpy()
+        # FP += ((preds != label).float() * (preds == 1).float()).sum(dim=(0,1)).cpu().data.numpy()
+        # TN += ((preds == label).float() * (preds == 0).float()).sum(dim=(0,1)).cpu().data.numpy()
+        # FN += ((preds != label).float() * (preds == 0).float()).sum(dim=(0,1)).cpu().data.numpy()
 
-        precision_per_label = TP / (TP + FP + 1e-10)
-        recall_per_label = TP / (TP + FN + 1e-10)
-        f1_per_label = 2 * precision_per_label * recall_per_label / (1e-5 + precision_per_label + recall_per_label)
+        # precision_per_label = TP / (TP + FP + 1e-10)
+        # recall_per_label = TP / (TP + FN + 1e-10)
+        # f1_per_label = 2 * precision_per_label * recall_per_label / (1e-5 + precision_per_label + recall_per_label)
 
-        writer.add_scaler('Train/loss', train_loss, epoch)
-        writer.add_scaler('Train/acc', train_acc, epoch)
-        writer.add_scaler('Validation/acc', val_acc, epoch)
-        writer.add_scalar("precision", precision_per_label, epoch)
-        writer.add_scalar("recall", recall_per_label, epoch)
-        writer.add_scalar("f1", f1_per_label, epoch)
+        writer.add_scalar('Train/loss', train_loss, epoch)
+        writer.add_scalar('Train/acc', train_acc, epoch)
+        writer.add_scalar('Validation/acc', val_acc, epoch)
+        # writer.add_scalar("precision", precision_per_label, epoch)
+        # writer.add_scalar("recall", recall_per_label, epoch)
+        # writer.add_scalar("f1", f1_per_label, epoch)
         for name, param in model.named_parameters():
             if not param.requires_grad:
                 continue
@@ -179,7 +193,7 @@ def train_network():
                 'optimizer_state_dict': optimizer.state_dict(),
                 'text_vocab': TEXT.vocab.stoi,
                 'label_vocab': LABEL.vocab.stoi
-            }, config['checkpoint_path'])
+            }, os.path.join(config['checkpoint_path'], config['outputmodelname']))
 
         # If validation accuracy does not improve, divide the learning rate by 5 and
         # if learning rate falls below 1e-5 terminate training
@@ -201,6 +215,16 @@ def train_network():
     else:
         print("\n" + "-"*100 + "\nMaximum epochs reached. Finished training !!")
 
+    print("\n" + "-"*50 + "\n\t\t\tEvaluating on test set\n" + "-"*50)
+    model_file = os.path.join(config['checkpoint_path'], config['outputmodelname'])
+    if os.path.isfile(model_file):
+        checkpoint = torch.load(model_file)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    else:
+        raise ValueError("\nNo Saved model state_dict found for the chosen model...!!! \nAborting evaluation on test set...".format(config['model_name']))
+    test_acc = eval_network(model, test = True)*100
+    print("\n" + "="*50 + "Test accuracy of best model = {:.2f}%".format(test_acc))
+
     writer.close()
     return None
 
@@ -216,7 +240,7 @@ if __name__ == '__main__':
                           help='path for Glove embeddings (850B, 300D)')
     parser.add_argument('--checkpoint_path', type = str, default = './checkpoints',
                           help='Directory of check point')
-    parser.add_argument("--outputmodelname", type=str, default= 'model.pickle', 
+    parser.add_argument("--outputmodelname", type=str, default= 'best_model.pt',
                        help = 'saved model name')
 
     # Training Params
@@ -245,7 +269,6 @@ if __name__ == '__main__':
     parser.add_argument('--max_epoch', type = int, default = 50,
                         help = 'Max epochs to train for')
 
-
     args, unparsed = parser.parse_known_args()
     config = args.__dict__
 
@@ -271,7 +294,9 @@ if __name__ == '__main__':
     # Prepare the tensorboard writer
     writer = SummaryWriter(os.path.join('logs', config['model_name']))
 
-    train_batch_loader, dev_batch_loader, TEXT, LABEL = prepare_training()
+    # Prepare the datasets and iterator for training and evaluation
+    train_batch_loader, dev_batch_loader, test_batch_loader, TEXT, LABEL = prepare_training()
+
     #Print args
     print("\nRunning training with the following parameters: \n" + "-"*50 + "\n")
     for key, value in config.items():
